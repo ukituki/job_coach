ab0c3538979f6b6aedf747848ca64dce810345e9
ukituki <ukituki@gmail.com>
2024-10-03 19:49:52 +0200

    debugging for deployment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   theme: {
   	container: {
-  		center: 'true',
+  		center: true,
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
